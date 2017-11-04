4160d0fb04b3c7944d45efab2faa147ec927fa8f
Cristian Anitei <christiannitas@gmail.com>
2017-11-04 13:32:12 +0200

    Update alpin.cpp

--- InfoArena/alpin.cpp
+++ InfoArena/alpin.cpp
@@ -1,80 +1,80 @@
-#include <fstream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define NMAX 1030
 using namespace std;
- 
-const int Nmax = 1024 + 2;
-const int Di[] = {-1, 0, 1, 0},
-          Dj[] = {0, 1, 0, -1};
- 
-ifstream cin ("alpin.in");
-ofstream cout ("alpin.out");
- 
-int N, A[Nmax][Nmax], DP[Nmax][Nmax];
-int FatherI[Nmax][Nmax], FatherJ[Nmax][Nmax], SourceI, SourceJ, Lmax;
- 
-void Input();
-int DFS(int i, int j);
-bool Ok(int i, int j);
-void Path(int i, int j);
- 
-int main()
-{
-    Input();
-    for (int i = 1, D; i <= N; ++i)
-        for (int j = 1; j <= N; ++j)
-        {
-            D = DFS(i, j);
-            if (D > Lmax)
-                Lmax = D, SourceI = i, SourceJ = j;
-        }
-  //  for (int i = 1; i <= N; ++i, cout << '\n')
-  //      for (int j = 1; j <= N; ++j)
-  //          cout << DP[i][j] << ' ';
- 
-    cout << Lmax << '\n';
-    Path(SourceI, SourceJ);
-}
- 
+
+ifstream fi("alpin.in"); void Input();
+ofstream fo("alpin.out"); void Output(int i, int j);
+int N;
+int fatheri[NMAX][NMAX], fatherj[NMAX][NMAX];
+int altitude[NMAX][NMAX], DP[NMAX][NMAX];
+int lmax = -1, sourcei, sourcej;
+int diri[] = {-1, 0, 1, 0};
+int dirj[] = { 0, 1, 0,-1};
+
+bool ok(int i, int j) {return i > 0 && j > 0 && i <= N && j <= N;}
+
 int DFS(int i, int j)
 {
-    if (!Ok(i, j)) return 0;
-    if (DP[i][j]) return DP[i][j];
- 
-    //cout << i << ' ' << j << '\n';
- 
-    for (int dir = 0, iv, jv, D; dir < 4; ++dir)
+    if(!ok(i,j)) return 0;
+    if(DP[i][j]) return DP[i][j];
+
+    for(int d = 0; d < 4; ++d)
     {
-        iv = i + Di[dir];
-        jv = j + Dj[dir];
-        if (A[iv][jv] > A[i][j])
+        int ii = i + diri[d];
+        int jj = j + dirj[d];
+        if(altitude[ii][jj] > altitude[i][j])
         {
-            D = DFS(iv, jv);
-            if (D > DP[i][j])
-                DP[i][j] = D, FatherI[i][j] = iv, FatherJ[i][j] = jv;
+            int k = DFS(ii,jj);
+            if(k > DP[i][j])
+            {
+                DP[i][j] = k;
+                fatheri[i][j] = ii; 
+                fatherj[i][j] = jj;
+            }
+
         }
     }
     DP[i][j]++;
     return DP[i][j];
-};
- 
-void Path(int i, int j)
+
+}
+
+int main()
 {
-    if (i == 0 || j == 0) return;
-    cout << i << ' ' << j << '\n';
-    Path(FatherI[i][j], FatherJ[i][j]);
-};
- 
-bool Ok(int i, int j)
-{
-    if (i < 1 || j < 1 || i > N || j > N) return 0;
-    return 1;
-};
- 
+    Input();
+    for(int i = 1; i <= N; ++i)
+    {
+        for(int j = 1; j <= N; ++j)
+        {
+            int d;
+            d = DFS(i,j);
+            if(d > lmax)
+            {
+                lmax = d;
+                sourcei = i;
+                sourcej = j;
+            }
+        }
+    }
+    fo<<lmax<<'\n';
+    Output(sourcei,sourcej);
+}
+
 void Input()
 {
-    cin >> N;
-    for (int i = 1; i <= N; ++i)
-        for (int j = 1; j <= N; ++j)
-            cin >> A[i][j];
-};
+    fi>>N;
+    for(int i = 1; i <= N; ++i)
+    {
+        for(int j = 1; j <= N; ++j)
+        {
+            fi>>altitude[i][j];
+        }
+    }
+}
+
+void Output(int i, int j)
+{
+    if(i == 0 || j == 0) return;
+    fo<<i<<' '<<j<<'\n';
+    Output(fatheri[i][j], fatherj[i][j]);
+}
